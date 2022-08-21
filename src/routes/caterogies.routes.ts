1badd0ced212a4113071ca6382363db015103cf5
ViktorHugoDev <viktorhugo.dev@gmail.com>
2022-08-21 11:33:40 -0300

    working with uploads

--- src/routes/caterogies.routes.ts
+++ src/routes/caterogies.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory/index'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 import { listCategoryController } from '../modules/cars/useCases/listCategory/index'
+
+const upload = multer({ dest: './tmp/' })
 
 const categoriesRoutes = Router()
 
@@ -12,4 +16,9 @@ categoriesRoutes.post('/', (request, response) => {
 categoriesRoutes.get('/', (request, response) => {
   return listCategoryController.handle(request, response)
 })
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response)
+})
+
 export { categoriesRoutes }
