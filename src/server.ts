9f44c3bed002bcf726fe0328d24ffc45ac561283
ViktorHugoDev <viktorhugo.dev@gmail.com>
2022-08-24 10:28:56 -0300

    Initial docker setup

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use(router)
 
-app.listen(3333, () => console.log('Running on port 3333'))
+app.listen(3333, () => console.log('Running on port 3333 on docker'))
