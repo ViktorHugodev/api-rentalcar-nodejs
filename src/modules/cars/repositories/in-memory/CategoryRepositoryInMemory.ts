d812fbf3fe82eab0bfc4c53f1c9b211743d8a03f
ViktorHugoDev <viktorhugo.dev@gmail.com>
2022-08-29 16:47:35 -0300

    add jest and config, tests for createCategory

---
+++ src/modules/cars/repositories/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,22 @@
+import { Category } from '../../entities/Category'
+import { ICaterogyRepository, IClassCategoryDTO } from '../ICategoryRepository'
+
+class CategoryRepositoryInMemory implements ICaterogyRepository {
+  categories: Category[] = []
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name)
+    return category
+  }
+  async list(): Promise<Category[]> {
+    const list = this.categories
+    return list
+  }
+  async create({ name, description }: IClassCategoryDTO): Promise<void> {
+    const category = new Category()
+    Object.assign(category, { name, description })
+    this.categories.push(category)
+  }
+}
+
+export { CategoryRepositoryInMemory }
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,50 @@
+
+
+
+import { AppErrors } from '../../../../errors/AppError'
+import { CategoryRepositoryInMemory } from '../../repositories/in-memory/CategoryRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoryRepositoryInMemory
+
+describe('Create category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoryRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    )
+  })
+  it('Should be able to create a new category', async () => {
+    const category = {
+      name: 'Category test',
+      description: 'Category test description',
+    }
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    })
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    )
+    console.log(categoryCreated)
+    expect(categoryCreated).toHaveProperty('id')
+  })
+  it('Should not be able to create a new category with the same name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category test',
+        description: 'Category test description',
+      }
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    }).rejects.toBeInstanceOf(AppErrors)
+  })
+})
