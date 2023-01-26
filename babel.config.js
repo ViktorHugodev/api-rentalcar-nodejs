0ad536f4b4fc97a91c1efc19c134b22f95d87b44
viktorhugodev <viktorhugo.dev@gmail.com>
2023-01-26 10:51:31 -0300

    fix: babel config

--- babel.config.js
+++ babel.config.js
@@ -7,12 +7,12 @@ module.exports = {
     [
       "module-resolver", {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@errors": ["./src/errors"],
-          "@utils": ["./src/utils"],
-          "@database": ["./src/database"]
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@errors": "./src/errors",
+          "@utils": "./src/utils",
+          "@database": "./src/database"
         }
       }
     ],
