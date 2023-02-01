3748c8552ae6d73fb2abbb05cf829aa7ce4712d3
viktorhugodev <viktorhugo.dev@gmail.com>
2023-02-01 10:21:55 -0300

    add: cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata'
+import cors from 'cors'
+import dotenv from 'dotenv'
 import express, { NextFunction, Request, Response } from 'express'
 import swaggerUi from 'swagger-ui-express'
-import 'reflect-metadata'
 
 import 'express-async-errors'
 
@@ -10,8 +12,6 @@ import upload from '@config/upload'
 import { AppErrors } from '@errors/AppError'
 import { createConnection } from '@shared/infra/database/data-source'
 import { router } from '@shared/infra/http/routes'
-
-import dotenv from 'dotenv'
 
 import swaggerConfig from './swagger.json'
 
@@ -23,6 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
